2b47d8d954eedb87b054b17bac6f0a5e062679ae
Bigyan Karki <karkibigyan05@gmail.com>
2026-05-23 09:18:26 +1000

    disable browser scroll restoration on refresh

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 import App from "./App.jsx";
 
+history.scrollRestoration = "manual";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
